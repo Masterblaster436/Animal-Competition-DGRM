2c00f5005fdb10058d421571c0598f9c91086e61
ga528970 <99818248+ga528970@users.noreply.github.com>
2022-04-22 10:07:05 -0400

    new

---
+++ animal.java
@@ -0,0 +1,52 @@
+public abstract class animals {
+  //public abstract int statistics();
+  
+  //public abstract String species();
+//%s
+ // The animals intelligence
+  
+  int Intel = 0;
+  public int getIntelLevel(){
+    return this.Intel;
+  }
+  
+  int Power = 0;
+  public int getPowerLevel(){
+    return this.Power;
+  }
+
+  int Defense = 0;
+  public int getDefenseLevel(){
+    return this.Defense;
+  }
+
+  int Mobility = 0;
+  public int getMobilityLevel(){
+    return this.Mobility;
+  }
+
+  int Stealth = 0;
+  public int getStealthLevel(){
+    return this.Stealth;
+  }
+
+  int Stamina = 0;
+  public int getStaminaLevel(){
+    return this.Stamina;
+  }
+
+  int Intimidation = 0;
+  public int getIntimidationLevel(){
+    return this.Intimidation;
+  }
+  
+  int Resolve = 0; 
+  public int getResolveLevel(){
+    return this.Resolve;
+  }
+  
+  int Total = 0;
+  public int getTotalLevel(){
+    return this.Total;
+  }
+}
